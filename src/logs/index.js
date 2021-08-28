f614d2e46cf40574a9c4f020f8a407c5701c7e85
João Augusto <joao.augusto@foodzilla.com.br>
2021-08-28 00:23:56 -0300

    fix: fixing credentials

--- src/logs/index.js
+++ src/logs/index.js
@@ -1,69 +1,74 @@
-const Sentry = require('@sentry/node')
-const { Severity, LoggerConfig, CoralogixLogger, Log } = require('coralogix-logger')
-const getRequestId = require('../getRequestId')
+const Sentry = require("@sentry/node");
+const {
+  Severity,
+  LoggerConfig,
+  CoralogixLogger,
+  Log,
+} = require("coralogix-logger");
+const getRequestId = require("../getRequestId");
 
 CoralogixLogger.configure(
   new LoggerConfig({
-    applicationName: 'application_name',
-    privateKey: 'private_key',
-    subsystemName: 'enviroment',
+    applicationName: "bk-moderation",
+    privateKey: "86807c6a-fd78-6edd-b6e3-9984194170c2",
+    subsystemName: process.env.NODE_ENV,
   })
-)
+);
 
-const logger = new CoralogixLogger('Logger')
+const logger = new CoralogixLogger("Logger");
 
 module.exports = {
   // Send warning log to Coralogix
   warn: (data) => {
-    const text = { data, request_id: getRequestId() }
+    const text = { data, request_id: getRequestId() };
 
-    console.log(text)
+    console.log(text);
 
     logger.addLog(
       new Log({
         severity: Severity.warning,
-        className: 'ConsoleLogger',
-        methodName: 'logger',
+        className: "ConsoleLogger",
+        methodName: "logger",
         text,
       })
-    )
+    );
   },
 
   // Send error log to Coralogix
   error: (data) => {
-    const text = { data, request_id: getRequestId() }
+    const text = { data, request_id: getRequestId() };
 
-    console.log(text)
+    console.log(text);
 
     logger.addLog(
       new Log({
         severity: Severity.error,
-        className: 'ConsoleLogger',
-        methodName: 'logger',
+        className: "ConsoleLogger",
+        methodName: "logger",
         text,
       })
-    )
+    );
 
-    Sentry.setTag('request_id', getRequestId())
-    Sentry.captureException(data)
+    Sentry.setTag("request_id", getRequestId());
+    Sentry.captureException(data);
   },
 
   // Send information log to Coralogix
   info: (data) => {
-    const text = { data, request_id: getRequestId() }
+    const text = { data, request_id: getRequestId() };
 
-    console.log(text)
+    console.log(text);
 
     logger.addLog(
       new Log({
         severity: Severity.info,
-        className: 'ConsoleLogger',
-        methodName: 'logger',
+        className: "ConsoleLogger",
+        methodName: "logger",
         text,
       })
-    )
+    );
   },
 
   // Transform object in string with json stringify
   beautify: (data) => JSON.stringify(data, undefined, 2),
-}
+};
